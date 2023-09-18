7b6dbbef5bd2ed395f99829e0b1ea9dd2cfd6482
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-18 12:45:38 -0300

    update: Formulario component added to code functions clienteSelecionado and clienteExcluido added to code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Botao from "@/components/Botao";
+import Formulario from "@/components/Formulario";
 import Layout from "@/components/Layout";
 import Tabela from "@/components/TabelaProps";
 import Cliente from "@/core/Cliente";
@@ -10,11 +12,25 @@ export default function Home() {
     new Cliente('Pedro', 4, '4')
 
   ]
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome)
+  }
+  function clienteExcluido(cliente: Cliente) {
+    console.log('Cliente nome:', cliente.nome)
+  }
+
   return (
     <div className="flex h-screen justify-center items-center bg-gradient-to-r from-blue-500 to-purple-500 text-white">
-      
+
       <Layout title="Cadastro simples">
-        <Tabela clientes={clientes}></Tabela>
+        <div className="flex justify-end">
+          <Botao className="mb-4 mt-4" cor="green">Novo cliente</Botao>
+        </div>
+
+        {/*<Tabela clientes={clientes} clienteExcluido={clienteExcluido} clienteSelecionado={clienteSelecionado}></Tabela>*/}
+        <Formulario cliente={clientes[2]}/>
+
       </Layout>
     </div>
   )
