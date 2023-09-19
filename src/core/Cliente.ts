b255ab6029cd37255cd5a49a7612da3c7a4ee025
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-19 09:53:10 -0300

    update: cliente.tsx id parameter updated

--- src/core/Cliente.ts
+++ src/core/Cliente.ts
@@ -3,7 +3,7 @@ export default class Cliente{
     #nome: string
     #idade: number
 
-    constructor(nome: string, idade: number , id: string = null){
+    constructor(nome: string, idade: number , id: string = ''){
         this.#nome = nome;
         this.#idade = idade;
         this.#id = id
