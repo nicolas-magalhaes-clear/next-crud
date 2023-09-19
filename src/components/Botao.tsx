1eac3b1a78b9ffa86b60ad765ff5516714b313a8
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-09-19 08:19:31 -0300

    update: onclick prop added to component

--- src/components/Botao.tsx
+++ src/components/Botao.tsx
@@ -10,7 +10,7 @@ export default function Botao(props: BotaoProps){
 
     const cor = props.cor ?? 'gray'
     return(
-        <button className={`
+        <button  onClick={props.onClick} className={`
         bg-gradient-to-r from-${cor}-400 to-${cor}-700 text-white px-4 py-2 rounded-md ${props.className}`}>
             {props.children}
         </button>
